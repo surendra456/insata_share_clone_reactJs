3d1d6bd39bce95768d9154f365eb822f13188cab
surendra456 <surendrasaladi456@gmail.com>
2022-02-28 15:41:52 +0000

    final_output

---
+++ src/components/Home/index..js
@@ -0,0 +1,32 @@
+import Header from '../Header'
+import Stories from '../Stories'
+import PostsList from '../PostsList'
+import SearchPostsList from '../searchPosts'
+import SearchContext from '../../SearchContext/index'
+import './index.css'
+
+const Home = () => (
+  <SearchContext.Consumer>
+    {value => {
+      const {searchInput, searchPostView} = value
+
+      return (
+        <>
+          <Header />
+          <div className="bg-color">
+            {searchPostView && searchInput !== '' ? (
+              <SearchPostsList input={searchInput} />
+            ) : (
+              <>
+                <Stories />
+                <PostsList />
+              </>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </SearchContext.Consumer>
+)
+
+export default Home
